ec065448880c766ccecb19ac4b30ccf801d723ea
ulvi1993 <ulvi.esedzadeh@gmail.com>
2021-07-28 08:51:43 +1300

    homework

---
+++ Homework/assets/main.js
@@ -0,0 +1,40 @@
+// let name = prompt("What is your name")
+// alert(name);
+
+
+// let student = confirm ("Are you student?")
+// alert(student);
+
+// let age = prompt("How old are you?");
+// if(age == 28) {
+//     alert("Yes you are 28");
+// } else{
+//     alert("No");
+// }
+
+// let allowed;
+// let age = prompt ("how old are you?");
+// if(age>18){
+//     allowed=true;
+// } else{
+//     allowed=false;
+// }
+// alert(allowed);
+
+// let age = prompt("how old are you")
+// let allowed = (age>18) ? true : false;
+// alert(allowed);
+
+let firstName = prompt("What is your name?");
+if(firstName=="Ulvi"){
+    let surname = prompt("what is your surname");
+    if(surname=="Asadzade"){
+        alert("Welcome Mr Ulvi Asadzade")
+    }
+    else{
+        alert("You are wrong Ulvi bro");
+    }
+}
+else{
+    alert("WTF Who are you???")
+}
